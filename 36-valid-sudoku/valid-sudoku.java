91bc7318ed768f2283a917e9f2cae9031d4feafb
Chinmay Barman <chinmaybarman0003@gmail.com>
2026-02-23 17:06:52 +0530

    Time: 3 ms (50.86%) | Memory: 46.4 MB (82.69%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -2,33 +2,29 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         Set<Character>row[]=new HashSet[9];
         Set<Character>col[]=new HashSet[9];
-        Set<Character>sub[][] =new HashSet[3][3];
+        Set<Character>sub[]=new HashSet[9];
         for(int i=0;i<9;i++){
             row[i]=new HashSet<>();
             col[i]=new HashSet<>();
+            sub[i]=new HashSet<>();
         }
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                sub[i][j]=new HashSet<>();
-            }
-        }
-        for(int r=0;r<board.length;r++){
-            for(int c=0;c<board[r].length;c++){
+        for(int r=0;r<9;r++){
+            for(int c=0;c<9;c++){
                 if(board[r][c]=='.'){
                     continue;
                 }
-                if(row[r].contains(board[r][c])){
+                else if(row[r].contains(board[r][c])){
                     return false;
                 }
                 else if(col[c].contains(board[r][c])){
                     return false;
                 }
-                else if(sub[r/3][c/3].contains(board[r][c])){
+                else if(sub[(r/3)*3+(c/3)].contains(board[r][c])){
                     return false;
                 }
                 row[r].add(board[r][c]);
                 col[c].add(board[r][c]);
-                sub[r/3][c/3].add(board[r][c]);
+                sub[(r/3)*3+(c/3)].add(board[r][c]);
             }
         }
         return true;
