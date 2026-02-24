e904c60517aeed71258afddf32ead2c0696e201a
Chinmay Barman <chinmaybarman0003@gmail.com>
2026-02-24 19:12:53 +0530

    Time: 11 ms (3.90%) | Memory: 67.6 MB (8.16%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,11 +1,11 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int arr[]=new int[nums.length];
+        HashMap<Integer,Integer>map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            arr[(i+k)%nums.length]=nums[i];
+            map.put(i,nums[i]);
         }
         for(int i=0;i<nums.length;i++){
-            nums[i]=arr[i];
+            nums[(i+k)%nums.length]=map.get(i);
         }
     }
 }
