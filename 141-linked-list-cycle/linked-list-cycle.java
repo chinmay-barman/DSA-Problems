ca07d742f122cb1803ca2abe8fc353d567250e15
Chinmay Barman <chinmaybarman0003@gmail.com>
2026-03-07 21:14:53 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (72.18%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,17 +11,17 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null){
+        if(head==null || head.next==null){
             return false;
         }
         ListNode slow = head;
-        ListNode fast = head.next.next;
+        ListNode fast = head;
         while(fast!=null && fast.next!=null){
-            if(fast==slow || fast.next==slow){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
                 return true;
             }
-            fast = fast.next.next;
-            slow = slow.next;
         }
         return false;
     }
