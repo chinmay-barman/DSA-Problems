03a781e919b68dcd0373f0aef09813bdbd5c2c3c
Chinmay Barman <chinmaybarman0003@gmail.com>
2026-03-19 23:39:01 +0530

    Time: 4 ms (73.27%) | Memory: 45 MB (72.77%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,16 +1,23 @@
 class Solution {
-    int findLargest(int nums2[], int index){
-        if(index == -1){
-            return -1;
-        }
-        int largest = -1;
-        for(int i=index+1;i<nums2.length;i++){
-            if(nums2[i]>nums2[index]){
-                largest = nums2[i];
-                break;
+    int[] largestNums2(int nums2[]){
+        int n = nums2.length;
+        int res[] = new int[n];
+        Stack<Integer>st = new Stack<>();
+        st.push(nums2[n-1]);
+        res[n-1] = -1;
+        for(int i=n-2;i>=0;i--){
+            while(st.size()>0 && st.peek()<nums2[i]){
+                st.pop();
             }
+            if(st.isEmpty()){
+                res[i] = -1;
+            }
+            else{
+                res[i] = st.peek();
+            }
+            st.push(nums2[i]);
         }
-        return largest;
+        return res;
     }
     int findIndex(int nums2[], int target){
         for(int i=0;i<nums2.length;i++){
@@ -21,11 +28,16 @@ class Solution {
         return -1;
     }
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int res2[] = largestNums2(nums2);
         int res[] = new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
-            int index = findIndex(nums2,nums1[i]);
-            int largest = findLargest(nums2, index);
-            res[i] = largest;
+            int index = findIndex(nums2, nums1[i]);
+            if(index!=-1){
+                res[i] = res2[index];
+            }
+            else{
+                res[i] = -1;
+            }
         }
         return res;
     }
