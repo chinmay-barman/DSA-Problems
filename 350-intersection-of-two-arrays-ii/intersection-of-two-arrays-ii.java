655b599a0f55a78d887f8244bd5714181dfe4153
Chinmay Barman <chinmaybarman0003@gmail.com>
2026-03-25 23:52:14 +0530

    Time: 3 ms (95.34%) | Memory: 44.9 MB (75.60%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,29 +1,31 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        ArrayList<Integer>s=new ArrayList<>();
-        HashMap<Integer, Integer>map=new HashMap<>();
+        HashMap<Integer,Integer>map = new HashMap<>();
+        ArrayList<Integer>res = new ArrayList<>();
         for(int i=0;i<nums1.length;i++){
             if(map.containsKey(nums1[i])){
-                int count=map.get(nums1[i]);
-                count+=1;
-                map.put(nums1[i], count);
+                int count = map.get(nums1[i]);
+                count++;
+                map.put(nums1[i],count);
             }
             else{
-                map.put(nums1[i], 1);
+                map.put(nums1[i],1);
             }
         }
         for(int i=0;i<nums2.length;i++){
-            if(map.containsKey(nums2[i]) && map.get(nums2[i])>0){
-                s.add(nums2[i]);
-                int count=map.get(nums2[i]);
-                count--;
-                map.put(nums2[i], count);
+            if(map.containsKey(nums2[i])){
+                int count = map.get(nums2[i]);
+                if(count!=0){
+                    count --;
+                    map.put(nums2[i],count);
+                    res.add(nums2[i]);
+                }
             }
         }
-        int arr[]=new int[s.size()];
-        for(int i=0;i<s.size();i++){
-            arr[i]=s.get(i);
+        int ans[] = new int[res.size()];
+        for(int i=0;i<res.size();i++){
+            ans[i] = res.get(i);
         }
-        return arr;
+        return ans;
     }
 }
