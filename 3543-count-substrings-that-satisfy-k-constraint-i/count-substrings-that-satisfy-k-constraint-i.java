590959e5b2c512afacbe3dcf93ba0737505c9078
Chinmay Barman <chinmaybarman0003@gmail.com>
2026-04-25 00:07:34 +0530

    Time: 2 ms (47.51%) | Memory: 43.7 MB (25.29%) - LeetSync

---
+++ 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countKConstraintSubstrings(String s, int k) {
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            int count0 = 0;
+            int count1 = 0;
+            for(int j=i;j<s.length();j++){
+                if(s.charAt(j)=='0'){
+                    count0++;
+                }
+                else{
+                    count1++;
+                }
+                if(count0>k && count1>k){
+                    break;
+                }
+                else if(count0<=k || count1<=k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
