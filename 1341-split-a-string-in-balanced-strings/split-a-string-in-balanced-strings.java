81baaf6db5eacb3a6fe84fdd6d77c91fd77839e1
Chinmay Barman <chinmaybarman0003@gmail.com>
2026-05-09 00:45:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (41.75%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int count = 0;
+        int countL = 0;
+        int countR = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='L'){
+                countL++;
+            }
+            else{
+                countR++;
+            }
+            if(countL==countR){
+                count++;
+                countL = 0;
+                countR = 0;
+            }
+        }
+        return count;
+    }
+}
